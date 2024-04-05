b4f85a9f94527c86fccf4849336e7d1470b4b0b2
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-04-05 22:44:32 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// let username;
+
+// document.getElementById('myBtn').onclick = () =>{
+//     username = document.getElementById('myText').value;
+//     document.getElementById('myH1').textContent = `Hello  ${ username }`;
+    
+
+// }
+
+// age =Number(window.prompt("what's the age of the soni?"));
+// // age = Number(age);
+// age += 1 ;
+// console.log(age);
+
+
+// #6 constants
+
+// const pi = 3.142;
+// let radius;
+// let circumference;
+
+// radius =  window.prompt("what is the radius of the circle");
+// radius = Number(radius);
+
+
+// circumference = 2 * pi * radius;
+
+//  console.log(circumference);
+
+// const pi = 3.142;
+// let radius;
+// let circumference;
+
+
+
+// document.getElementById("myBtn").onclick = ()=> {
+
+//    radius = document.getElementById("myText").value;
+//    radius = Number(radius);
+//    circumference = 2 * pi * radius;
+
+//    document.getElementById("myH3").textContent = `The circumference is  = ${circumference} cm` ;
+
+
+
+ 
+// }
+
+
+// #7 increment and decrement Task
+
+
